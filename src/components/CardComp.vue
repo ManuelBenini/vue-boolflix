262ca60b523feb0f5881805a55848e45ccbf82f0
ManuelBenini <manuelbenini1905@gmail.com>
2022-05-22 17:46:43 +0200

    Struttura base

---
+++ src/components/CardComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    CARD
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComp'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    MAIN
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MainComp'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
